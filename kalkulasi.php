3fc4ddb28197aec80556c9c873147da0ef69ca0d
Sukedi <169124018+Sukedi@users.noreply.github.com>
2025-03-05 13:21:55 +0700

    Delete kalkulasi.php

--- kalkulasi.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *"); // Izinkan akses dari GitHub Pages
-header("Content-Type: application/json");
-
-$angka = $_GET['angka'] ?? 0;
-$hasil = $angka * 2; // Kalkulasi rahasia
-
-echo json_encode(["hasil" => $hasil]);
-?>
